1ed022b398bb06f377f32d5e85ea7a4a4b292d75
chenzujie <chenzujie@91yong.com>
2016-10-27 11:16:43 +0800

    学习自定义导航栏

--- index.android.js
+++ index.android.js
@@ -53,9 +53,14 @@
 // AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
 import React, { Component } from 'react';
 import {AppRegistry, StyleSheet, Text,Image, View, TextInput,
-    ScrollView, ListView, TouchableOpacity, Navigator} from 'react-native';
+    ScrollView, ListView, TouchableOpacity, Navigator, NavigationExperimental, PixelRatio, TouchableHighlight} from 'react-native';
 import Button from './src/component/button';
 import MyScene from  './src/MyScene';
+
+const {
+    CardStack: NavigationCardStack,
+    StateUtils: NavigationStateUtils,
+} = NavigationExperimental;
 
 class HelloWorldApp extends Component{
   render(){
@@ -477,6 +482,115 @@ class SimpleNavigationApp extends Component {
     }
 }
 
+class TappableRow extends Component{
+    render(){
+        return(
+            <TouchableHighlight
+                style={styles.row}
+                underlayColor="#D0D0D0"
+                onPress={this.props.onPress}
+                >
+                <Text style={styles.buttonText}>
+                    {this.props.text}
+                </Text>
+            </TouchableHighlight>
+        );
+    }
+}
+
+class MyVeryComplexScene extends Component{
+    render(){
+        return(
+            <ScrollView style={styles.scrollView}>
+                <Text style={styles.row}>
+                    Route:{this.props.route.key}
+                </Text>
+                <TappableRow
+                    text="Tap me to load the next scene"
+                    onPress={this.props.onPushRoute}
+                    />
+                <TappableRow
+                    text="Tap me to go back"
+                    onPress={this.props.onPopRoute}
+                    />
+            </ScrollView>
+        );
+    }
+}
+
+class MyVerySimpleNavigator extends Component{
+    constructor(props, context){
+        super(props, context);
+        this._onPushRoute = this.props.onNavigationChange.bind(null, 'push');
+        this._onPopRoute = this.props.onNavigationChange.bind(null, 'pop');
+        this._renderScene = this._renderScene.bind(this);
+    }
+
+    render(){
+        return(
+            <NavigationCardStack
+                onNavigateBack={this._onPopRoute}
+                navigationState={this.props.navigationState}
+                renderScene={this._renderScene}
+                style={styles.navigator}
+            />
+        );
+    }
+
+    _renderScene(sceneProps){
+        return(
+            <MyVeryComplexScene
+                route={sceneProps.scene.route}
+                onPushRoute={this._onPushRoute}
+                onPopRoute={this._onPopRoute}
+                onExit={this.props.onExit}
+            />
+        );
+    }
+}
+
+
+class BleedingEdgeApplication extends Component{
+    constructor(props, context){
+        super(props, context);
+        this.state={
+            navigationState:{
+                index: 0,
+                routes: [{key: 'My Initial Scene'}],
+            }
+        };
+
+        this._onNavigationChange = this._onNavigationChange.bind(this);
+    }
+
+    _onNavigationChange(type){
+        let {navigationState} = this.state;
+        switch (type){
+            case 'push':
+                const route = {key: 'Route-' + Date.now()}
+                navigationState = NavigationStateUtils.push(navigationState, route);
+                break;
+            case 'pop':
+                navigationState = NavigationStateUtils.pop(navigationState);
+                break;
+        }
+        if(this.state.navigationState != navigationState){
+            this.setState({navigationState});
+        }
+    }
+
+    render(){
+        return (
+            <MyVerySimpleNavigator
+                navigationState={this.state.navigationState}
+                onNavigationChange={this._onNavigationChange}
+                onExit={this._exit}
+            />
+        );
+    }
+}
+
+
 const styles = StyleSheet.create({
     bigblue:{
       color:'blue',
@@ -485,8 +599,27 @@ const styles = StyleSheet.create({
     },
   red:{
     color:'red',
-   }
+   },
+    navigator:{
+      flex: 1,
+    },
+    scrollView:{
+        marginTop:64
+    },
+    row:{
+        padding: 15,
+        backgroundColor: 'white',
+        borderBottomWidth: 1/PixelRatio.get(),
+        borderBottomColor: '#CDCDCD'
+    },
+    rowText:{
+        fontSize: 17
+    },
+    buttonText:{
+        fontSize: 17,
+        fontWeight: '500'
+    }
   }
 );
 
-AppRegistry.registerComponent('HelloWorld', () => SimpleNavigationApp);
+AppRegistry.registerComponent('HelloWorld', () => BleedingEdgeApplication);
