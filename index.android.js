5bc445a71d38fdc46f3b1228bb7d0a8b08c23143
chenzujie <chenzujie@91yong.com>
2016-10-30 18:00:23 +0800

    ViewPagerAndroid 学习

--- index.android.js
+++ index.android.js
@@ -53,9 +53,11 @@
 // AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
 import React, { Component } from 'react';
 import {AppRegistry, StyleSheet, Text,Image, View, TextInput,
-    ScrollView, ListView, TouchableOpacity, Navigator, NavigationExperimental, PixelRatio, TouchableHighlight} from 'react-native';
+    ScrollView, ListView, TouchableOpacity, Navigator, NavigationExperimental, PixelRatio, TouchableHighlight, TouchableNativeFeedback,
+    TouchableOpacity, ViewPagerAndroid} from 'react-native';
 import Button from './src/component/button';
 import MyScene from  './src/MyScene';
+import type, { ViewPagerScrollState } from 'ViewPagerAndroid';
 
 const {
     CardStack: NavigationCardStack,
@@ -590,6 +592,228 @@ class BleedingEdgeApplication extends Component{
     }
 }
 
+class TouchableHighlightButton extends Component {
+    _onPressButton() {
+        console.log("You tapped the button!");
+    }
+
+    render() {
+        return (
+            <TouchableHighlight onPress={this._onPressButton}>
+                <Text>Button</Text>
+            </TouchableHighlight>
+        );
+    }
+}
+
+class TouchableNativeFeedbackButton extends Component {
+    _onPressButton() {
+        console.log("You tapped the button!");
+    }
+
+    render() {
+        return (
+            <TouchableNativeFeedback
+                onPress={this._onPressButton}
+                background={TouchableNativeFeedback.SelectableBackground()}>
+                <View style={{width: 150, height: 100, backgroundColor: 'red'}}>
+                    <Text style={{margin: 30}}>Button</Text>
+                </View>
+            </TouchableNativeFeedback>
+        );
+    }
+}
+
+var PAGES = 5;
+var BGCOLOR = {'#fdc08e','#fff6b9','#99d1b7','#dde5fe','#f79273'};
+var IMAGE_URIS = ['http://apod.nasa.gov/apod/image/1410/20141008tleBaldridge001h990.jpg',
+    'http://apod.nasa.gov/apod/image/1409/volcanicpillar_vetter_960.jpg',
+    'http://apod.nasa.gov/apod/image/1409/m27_snyder_960.jpg',
+    'http://apod.nasa.gov/apod/image/1409/PupAmulti_rot0.jpg',
+    'http://apod.nasa.gov/apod/image/1510/lunareclipse_27Sep_beletskycrop4.jpg'];
+
+class LikeCount extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            likes: 7
+        };
+    }
+
+    onClick = () =>{
+        this.setState({
+            likes: this.state.likes + 1
+        });
+    }
+
+    render(){
+        var thumbsUp = '\uD83D\uDC4D';
+        return(
+            <View style={styles.likeContainer}>
+                <TouchableOpacity onPress={this.onClick.bind(this)} styles={styles.likeButton}>
+                    <Text style={styles.likesText}>
+                        {thumbsUp + 'like'}
+                    </Text>
+                </TouchableOpacity>
+                <Text style={styles.likesText}>
+                    {this.state.likes + ' likes'}
+                </Text>
+            </View>
+        );
+    }
+}
+
+class Button extends Component{
+    _handlePress = () =>{
+        if(this.props.enabled && this.props.onPress){
+            this.props.onPress();
+        }
+    }
+
+    render(){
+        return(
+            <TouchableWithoutFeedback onPress={this._handlePress.bind(this)}>
+                <View style={[styles.button, this.props.enabled? {} : styles.buttonDisabled]}>
+                    <Text style={styles.buttonText}>{this.props.text}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+class ProgressBar extends Component{
+    render(){
+        var fractionPosition = (this.props.progress.position + this.props.progress.offset);
+        var progressBarSize = (fractionPosition / (PAGES - 1)) * this.props.size;
+        return (
+            <View style={[styles.progressBarContainer, {width: this.props.size}]}>
+                <View style={[styles.progressBar, {width: progressBarSize}]}/>
+            </View>
+        );
+    }
+}
+
+class ViewPagerAndroidExample extends Component{
+    statics={
+        title: '<ViewPagerAndroid>',
+        description: 'Container that allows to flip left and right between child views.'
+    }
+
+    constructor(props){
+        super(props);
+        this.state={
+            page: 0,
+            animationsAreEnabled: true,
+            scrollEnabled: true,
+            progress:{
+                position: 0,
+                offset: 0,
+            }
+        }
+    }
+
+    onPageSelected = (e)=>{
+        this.setState({
+            page: e.nativeEvent.position
+        });
+    }
+
+    onPageScroll = (e) =>{
+        this.setState({
+            progress: e.nativeEvent
+        });
+    }
+
+    onPageScrollStateChanged = (state: ViewPagerScrollState) => {
+        this.setState({
+            scrollState: state
+        });
+    }
+
+    move = (delta) => {
+        var page = this.state.page + delta;
+        this.go(page);
+    }
+
+    go = (page) =>{
+        if(this.state.animationsAreEnabled){
+            this.viewPager.setPage(page);
+        }
+        else {
+            this.viewPager.setPageWidthoutAnimation(page);
+        }
+        this.setState({page});
+    }
+
+    render(){
+        console.log(1);
+        var pages = [];
+        for(var i = 0; i < PAGES; i++){
+            var pageStyle = {
+                backgroundColor: BGCOLOR[i % BGCOLOR.length],
+                alignItems: 'center',
+                padding: 20
+            };
+            pages.push(
+              <View key={i} style={pageStyle} collapsable={false}>
+                  <Image style={styles.image}
+                    source={{uri: IMAGE_URIS[i % BGCOLOR.length]}}
+                  />
+                  <LikeCount />
+              </View>
+            );
+        }
+        var {page, animationAreEnabled} = this.state;
+        return (
+            <View style={styles.container}>
+                <ViewPagerAndroid
+                    style={styles.viewPaper}
+                    initialPage={0}
+                    scrollEnabled={this.state.scrollEnabled}
+                    onPageScroll={this.onPageScroll.bind(this)}
+                    onPageSelected={this.onPageSelected.bind(this)}
+                    onPageScrollStateChanged={this.onPageScrollStateChanged.bind(this)}
+                    pageMargin={10}
+                    ref = {viewPager => {this.viewPager = viewPager}}>
+                    {pages}
+                </ViewPagerAndroid>
+                <View style={styles.buttons}>
+                    <Button
+                        enabled={true}
+                        text={this.state.scrollEnabled? 'Scroll Enable': 'Scroll Disable'}
+                        onPress={()=>{
+                            this.setState({
+                                scrollEnabled: !this.state.scrollEnabled
+                            });
+                        }}
+                        />
+                </View>
+                <View style={styles.buttons}>
+                    { animationsAreEnabled ?
+                        <Button
+                            text="Turn off animations"
+                            enabled={true}
+                            onPress={() => this.setState({animationsAreEnabled: false})}
+                        /> :
+                        <Button
+                            text="Turn animations back on"
+                            enabled={true}
+                            onPress={() => this.setState({animationsAreEnabled: true})}
+                        /> }
+                    <Text style={styles.scrollStateText}>ScrollState[ {this.state.scrollState} ]</Text>
+                </View>
+                <View style={styles.buttons}>
+                    <Button text="Start" enabled={page > 0} onPress={() => this.go(0)}/>
+                    <Button text="Prev" enabled={page > 0} onPress={() => this.move(-1)}/>
+                    <Text style={styles.buttonText}>Page {page + 1} / {PAGES}</Text>
+                    <ProgressBar size={100} progress={this.state.progress}/>
+                    <Button text="Next" enabled={page < PAGES - 1} onPress={() => this.move(1)}/>
+                    <Button text="Last" enabled={page < PAGES - 1} onPress={() => this.go(PAGES - 1)}/>
+                </View>
+            </View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     bigblue:{
@@ -615,11 +839,76 @@ const styles = StyleSheet.create({
     rowText:{
         fontSize: 17
     },
+    // buttonText:{
+    //     fontSize: 17,
+    //     fontWeight: '500'
+    // },
+    buttons:{
+        flexDirection: 'row',
+        height: 30,
+        backgroundColor: 'black',
+        alignItems: true,
+        justifyContent: true
+    },
+    button:{
+        flex: 1,
+        width:0,
+        margin: 5,
+        borderColor: 'gray',
+        borderWidth: 1,
+        backgroundColor: 'gray'
+    },
+    buttonDisabled:{
+        backgroundColor: 'black',
+        opacity: 0.5
+    },
     buttonText:{
-        fontSize: 17,
-        fontWeight: '500'
+        color: 'white'
+    },
+    scrollStateText:{
+        color: '@99d1b7'
+    },
+    container:{
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    image:{
+        width:300,
+        width:200,
+        padding:20
+    },
+    likeButton:{
+        backgroundColor: 'rgba(0,0,0,0.1)',
+        borderColor: '#333333',
+        borderWidth: 1,
+        borderRadius: 5,
+        flex: 1,
+        margin: 8,
+        padding: 8
+    },
+    likeContainer:{
+        flexDirection: 'row',
+    },
+    likesText:{
+        flex: 1,
+        fontSize: 18,
+        alignSelf: 'center'
+    },
+    progressBarContainer:{
+        height: 10,
+        margin: 10,
+        borderColor: '#eeeeee',
+        borderWidth: 2
+    },
+    progressBar: {
+        alignSelf: 'flex-start',
+        flex: 1,
+        backgroundColor: '#eeeeee'
+    },
+    viewPaper:{
+        flex:1
     }
   }
 );
 
-AppRegistry.registerComponent('HelloWorld', () => BleedingEdgeApplication);
+AppRegistry.registerComponent('HelloWorld', () => ViewPagerAndroidExample);
